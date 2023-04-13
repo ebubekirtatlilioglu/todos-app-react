de2e0e3f52cc74b8ec35f95dd3af1ae88a2f1ac7
ebubekirtatlilioglu <tatlilioglu_ebubekir@hotmail.com>
2023-04-13 10:08:17 +0300

    publish

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export"//her host hesapta çalışabilmesi için bu komutu ekledik
 }
 
 module.exports = nextConfig
